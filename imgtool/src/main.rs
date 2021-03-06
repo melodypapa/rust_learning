1229ff28d7df7202747a71121bec6de442521cf9
Melodypapa <melodypapa@outlook.com>
2021-03-06 21:43:02 +0800

    add the implementation for the imgtool

--- imgtool/src/main.rs
+++ imgtool/src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let image_path = env::args().next().unwrap();
+    let path = Path::new(&image_path);
+    let img = image::open(path).unwrap();
+    let rotated = img.rotate90();
+    rotated.save(path).unwrap();
+
+    println!("{:?}",path);
 }
