8221cf2b2dc325cc90cc76686c615d1e5b4dd43e
Melodypapa <melodypapa@outlook.com>
2021-03-08 13:07:37 +0800

    add the formatted print debug

---
+++ rust_examples/formatted_print_debug/src/main.rs
@@ -0,0 +1,15 @@
+#[derive(Debug)]
+struct Structure(i32);
+
+fn main() {
+    println!("{:?} months in a year.", 12);
+
+    println!(
+        "{1:?} {0:?} is the {actor:?} name",
+        "Slater",
+        "Christian",
+        actor = "actor's"
+    );
+
+    println!("Now {:?} will print!", Structure(3));
+}
