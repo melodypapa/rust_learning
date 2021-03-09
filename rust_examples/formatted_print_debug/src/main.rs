ae7362e1a06de518d6ee88cc6cfd7f2c9fa51087
Melodypapa <melodypapa@outlook.com>
2021-03-09 13:01:47 +0800

    add the fomatted print debug source

--- rust_examples/formatted_print_debug/src/main.rs
+++ rust_examples/formatted_print_debug/src/main.rs
@@ -1,6 +1,15 @@
 #[derive(Debug)]
 struct Structure(i32);
 
+#[derive(Debug)]
+struct Deep(Structure);
+
+#[derive(Debug)]
+struct Person<'a> {
+    name: &'a str,
+    age: u8
+}
+
 fn main() {
     println!("{:?} months in a year.", 12);
 
@@ -12,4 +21,13 @@ fn main() {
     );
 
     println!("Now {:?} will print!", Structure(3));
+
+    println!("Now {:?} will print!", Deep(Structure(7)));
+
+    let name = "Peter";
+    let age = 27;
+    let peter = Person { name, age };
+
+    // Pretty print
+    println!("{:#?}", peter);
 }
