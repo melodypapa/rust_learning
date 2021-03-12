eb869eb0bfc785f53ffc8d5faab05a0c6a06ec2b
Melodypapa <melodypapa@outlook.com>
2021-03-12 13:12:39 +0800

    add the examples

--- rust_examples/primitives_literals_and_operators/src/main.rs
+++ rust_examples/primitives_literals_and_operators/src/main.rs
@@ -1,27 +1,21 @@
-fn main() {
-    let logical: bool = true;
+fn main(){
+    println!("1 + 2 = {}", 1u32 + 2);
 
-    let a_float: f64 = 1.0;
-    let an_integer = 5i32;
+    println!("1 - 2 = {}", 1i32 - 2);
 
-    let default_float = 3.0;
-    let default_integer = 7;
+    println!("true AND false is {}", true && false);
 
-    let mut inferred_type = 12;
-    inferred_type = 4294967296i64;
+    println!("true OR false is {}", true || false);
 
-    let mut mutable = 12;
-    mutable = 21;
+    println!("NOT true is {}", !true);
 
-    // mutable = true;
+    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
 
-    let mutable = true;
+    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
 
-    println!("logical = {}", logical);
-    println!("a_float = {}", a_float);
-    println!("an_integer = {}", an_integer);
-    println!("default_float = {}", default_float);
-    println!("default_integer = {}", default_integer);
-    println!("inferred_type = {}", inferred_type);
-    println!("mutable = {}", mutable);
+    println!("1 << 5 is {}", 1u32 << 5);
+
+    println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
+
+    println!("One million is written as {}", 1_000_000u32);
 }
---
+++ rust_examples/primitives_tuples/src/main.rs
@@ -0,0 +1,11 @@
+fn reverse(pair: (i32, bool)) -> (bool, i32){
+    let (integer, boolean) = pair;
+
+    (boolean, integer)
+}
+
+
+
+fn main() {
+    println!("Hello, world!");
+}
